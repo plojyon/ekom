a3340889db63f523b81f7ce16c816504e1a063d8
plojyon <yon.ploj@gmail.com>
2020-01-31 16:11:53 +0100

    moved js to own file & added icons to procedurally generated submissions

---
+++ generate_submissions.js
@@ -0,0 +1,113 @@
+icon_link = "https://upload.wikimedia.org/wikipedia/commons/thumb/8/87/PDF_file_icon.svg/1200px-PDF_file_icon.svg.png";
+submissions = [
+	{
+		"title": "Deutsche grammatik oder wendische Sprache",
+		"tags": ["Grammatik", "Prateritum", "Lehrbuch"],
+		"author": "yon.je.ploj",
+		"professor": "Prof. Vojsk"
+	},
+	{
+		"title": "Kuharske bukve",
+		"tags": ["kuharski vuk", "bukve", "hrana"],
+		"author": "vodnik.valentin",
+		"professor": "Prof. Ceklin"
+	},
+	{
+		"title": "Programiranje z Javo ali Zakaj so moje ocene slabe",
+		"tags": ["Ta nesrecni dan", "Maticek se zeni", "C/C++"],
+		"author": "spontana geneza datoteke",
+		"professor": "Prof. Burger"
+	},
+	{
+		"title": "Kemija v 1. letniku",
+		"tags": ["Kisline, baze in soli", "Poimenovanje", "Periodni sistem"],
+		"author": "masa.kovac",
+		"professor": "Prof. Cebin"
+	},
+	{
+		"title": "Moj zvezk",
+		"tags": [],
+		"author": "yon.ploj",
+		"professor": "Prof. Levstek"
+	},
+	{
+		"title": "Tvoj zvezk",
+		"tags": ["lep in urejen"],
+		"author": "yon.ploj",
+		"professor": "Prof. Levstek"
+	},
+	{
+		"title": "Recept za palacinke",
+		"tags": ["moka", "jajca", "mleko", "sladkor", "nutella"],
+		"author": "namazes z Nutello",
+		"professor": "speces u ponvi"
+	},
+	{
+		"title": "Kaj vse mam na mizi?",
+		"tags": ["zvezke", "cokolado", "racunalnik", "kozarce", "kroznike", "slusalke", "pisala", "domaco nalogo", "drobtine", "umazano zlico", "smeti", "vodko"],
+		"author": "sej bom pospravu",
+		"professor": "untidy geng"
+	},
+];
+
+window.onload = function() {
+	submissions.forEach(createSubmission);
+}
+
+function applyTagFilter(tag) {
+	console.log(tag);
+}
+
+function createSubmission(sub, index, arr) {
+
+	img = document.createElement("img");
+	img.src = icon_link;
+	img_div = document.createElement("div");
+	img_div.appendChild(img);
+
+	title = document.createElement("a");
+	title.classList.add("title");
+	title.href = "http://google.com";
+	title.innerText = sub.title;
+
+	tags = document.createElement("p");
+	tags.classList.add("tags");
+	for (tag in sub.tags) {
+		t = document.createElement("a");
+		t.classList.add("tag");
+		t.innerText = sub.tags[tag];
+		t.onclick = ((self) => { applyTagFilter(self.target.text); });
+		tags.appendChild(t);
+		tags.appendChild(document.createTextNode(" "));
+	}
+
+	prof_icon = document.createElement("i");
+	prof_icon.classList.add("fas");
+	prof_icon.classList.add("fa-chalkboard-teacher");
+	professor = document.createElement("p");
+	professor.classList.add("professor");
+	professor.appendChild(prof_icon);
+	professor.appendChild(document.createTextNode(" "+sub.professor));
+
+	auth_icon = document.createElement("i");
+	auth_icon.classList.add("fas");
+	auth_icon.classList.add("fa-pencil-alt");
+	author = document.createElement("p");
+	author.classList.add("author");
+	author.appendChild(auth_icon);
+	author.appendChild(document.createTextNode(" "+sub.author));
+
+	text_div = document.createElement("div");
+	text_div.classList.add("text");
+	text_div.appendChild(title);
+	text_div.appendChild(tags);
+	text_div.appendChild(professor);
+	text_div.appendChild(author);
+
+	submission = document.createElement("div");
+	submission.classList.add("submission");
+	submission.appendChild(img_div);
+	submission.appendChild(text_div);
+
+	document.getElementById("grid").appendChild(submission);
+}
