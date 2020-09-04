dbaa5fa04410967a7c2691d3633d0882359dd996
plojyon <yon.ploj@gmail.com>
2020-09-04 16:52:48 +0200

    added upload logic

---
+++ services/upload.php
@@ -0,0 +1,11 @@
+<?php
+//$file = $_POST["upload_file"];
+echo print_r($_POST);
+echo "<br>";
+echo "<br>";
+echo print_r($_GET);
+echo "<br>";
+echo "<br>";
+echo print_r($_FILES);
+//file_put_contents("accepted.gif", $file);
+?>
