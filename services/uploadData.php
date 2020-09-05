6ceab91f2752a28983d510ee85a71d9dda79d605
urbancluka <urbancluka3@gmail.com>
2020-09-05 15:48:25 +0200

    fixed sql code

--- services/uploadData.php
+++ services/uploadData.php
@@ -5,6 +5,14 @@
 			 VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
 	$statement = $conn->prepare($ukaz);
+	$statement->bind_param('sssissss', $_POST[author],
+	 								   $_POST[professor],
+									   $_POST[predmet],
+									   $_POST[letnik],
+									   $_POST[tags],
+									   $_POST[date],
+									   $_POST[title],
+									   $_POST[filename]);
 	$statement->execute();
 	echo json_encode($data);
 	$conn->close();
