4c877060639c18f76fb4171d22b209b02cfdf24d
urbancluka <urbancluka3@gmail.com>
2020-09-05 15:36:58 +0200

    changed upload names, fixed insert

--- services/uploadData.php
+++ services/uploadData.php
@@ -1,27 +1,11 @@
 <?php
 	include("../../credentials.php");
 
-	$ukaz = "INSERT INTO submissions (AUTHOR, PROFESSOR, PREDMET, LETNIK, TAGS, DATE, TITLE, FILENAME); 
-			 VALUES (author, professor, predmet, letnik, tags, date, title, filename)";
+	$ukaz = "INSERT INTO submissions (AUTHOR, PROFESSOR, PREDMET, LETNIK, TAGS, DATE, TITLE, FILENAME)
+			 VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
 	$statement = $conn->prepare($ukaz);
 	$statement->execute();
-	$statement->store_result();
-	$rows = $statement->num_rows;
-	$statement->bind_result();
-	$data = array();
-	$i = 0;
-	while ($statement->fetch()) {
-		$data[$i]= array("author" => $author,
-						 "professor" => $professor,
-						 "predmet" => $predmet,
-						 "letnik" => $letnik,
-						 "tags" => explode(",", $tags),
-						 "date" => $date,
-						 "title" => $title,
-						 "filename" => $filename);
-		$i++;
-	}
 	echo json_encode($data);
 	$conn->close();
 
