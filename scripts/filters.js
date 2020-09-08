8312e39039f901fa2b090bd639b19c20a667769b
plojyon <yon.ploj@gmail.com>
2020-09-08 17:42:42 +0200

    refresh_filters works without verified checkbox

--- scripts/filters.js
+++ scripts/filters.js
@@ -4,16 +4,16 @@ function applyTagFilter(tag) {
 function refresh_filters() {
 	 submissions.forEach((submission, i) => {
 	 	var filter_letnik = document.getElementById("letnik_select").value;
-	 	var filter_verified = document.getElementById("verified_checkbox").value;
+	 	//var filter_verified = document.getElementById("verified_checkbox").value;
 	 	var filter_predmet = document.getElementById("predmet_select").value;
 	 	var filter_professor = document.getElementById("professor_select").value;
 		var submission_letnik = submission ["letnik"];
-		/*var submission_verified = submission ["verified"];*/
+		//var submission_verified = submission ["verified"];
 		var submission_predmet = submission ["predmet"];
 		var submission_professor = submission ["professor"];
 		var elementname = document.getElementById(submission ["filename"]);
 		if ((filter_letnik == submission_letnik || filter_letnik == "") &&
-			/*filter_verified == submission_verified &&*/
+			//filter_verified == submission_verified &&//
 			(filter_predmet == submission_predmet || filter_predmet == "") &&
 			(filter_professor == submission_professor || filter_professor == "")) {
 			elementname.classList.remove("hidden");
