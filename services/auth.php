07ffbc29737d2c3fcbd20a385ff5bdb1864f7741
plojyon <yon.ploj@gmail.com>
2020-09-20 17:08:40 +0200

    finalized auth page

--- services/auth.php
+++ services/auth.php
@@ -1,52 +1,32 @@
 <?php
 
-include("../../credentials.php");
+	include("../../credentials.php");
 
-$ukaz = "SELECT * FROM prof_credentials WHERE username = ?";
+	$ukaz = "SELECT * FROM prof_credentials WHERE username = ?";
 
-$statement = $conn->prepare($ukaz);
-$statement->bind_param('s', $_POST["uname"]);
-$statement->execute();
-$statement->store_result();
-$statement->bind_result($username, $password);
-while ($statement->fetch()) {
-	if ($username == $_POST["uname"] && $password == $_POST["pword"]) {
-		echo "prijava uspesna<br>";
-		$code = rand(100000, 999999);
-		echo $code;
-		$statement->close();
+	$statement = $conn->prepare($ukaz);
+	$statement->bind_param('s', $_POST["uname"]);
+	$statement->execute();
+	$statement->store_result();
+	$statement->bind_result($username, $password);
+	while ($statement->fetch()) {
+		if ($username == $_POST["uname"] && $password == $_POST["pword"]) {
+			echo "Prijava uspešna.<br>";
+			$code = rand(100000, 999999);
+			echo "Ustvarjena koda: " . $code;
+			$statement->close();
 
-		$statement = $conn->prepare("INSERT INTO codes (code,comment) VALUES (?,?)");
-		$statement->bind_param('is', $code,$_POST["comment"]);
-		$statement->execute();
+			$statement = $conn->prepare("INSERT INTO codes (code,comment) VALUES (?,?)");
+			$statement->bind_param('is', $code,$_POST["comment"]);
+			$statement->execute();
 
-		exit();
+			echo '<br><br><a href="../index.html">Domov</a>';
+			exit();
+		}
 	}
-}
-echo "ni ok, poglej se enkrt";
-$statement->close();
+	echo "Neveljavno uporabniško ime ali geslo.";
+	$statement->close();
 
-/*1. nardis $conn (include())
-2. nardis statement
-    $statement = $conn->prepare("SQL ukaz");
-3. vstavis parametre
-    $statement->bind_param('ss', $address, $receiver);
-4. pozenes ukaz
-    $statement->execute();
-5. shrans rezultat
-    $statement->store_result();
-6. pogledas st vrstic
-    $rows = $statement->num_rows;
-7. shranis rezultate
-    $statement->bind_result($addr, $bal);
-8. obdelava rezultatov
-    while ($statement->fetch()) {}
-9. zapres ukaz
-    $statement->free_result();
-    $statement->close();
-10. nazaj na 2. (po portebi)
-11. disconnect from database
-    $conn->close();*/
 ?>
 <br>
 <a href="../index.html">Domov</a>
--- services/upload.php
+++ services/upload.php
@@ -1,101 +1,102 @@
 <?php
 
-include("../../credentials.php");
+	include("../../credentials.php");
 
-$predmeti = [
-	"ang",
-	"bio",
-	"fil",
-	"fiz",
-	"fra",
-	"geo",
-	"gla",
-	"inf",
-	"ita",
-	"kem",
-	"lum",
-	"mat",
-	"nem",
-	"ped",
-	"psh",
-	"rus",
-	"slo",
-	"soc",
-	"spa",
-	"svz",
-	"uzg",
-	"zgo"
-];
+	$predmeti = [
+		"ang",
+		"bio",
+		"fil",
+		"fiz",
+		"fra",
+		"geo",
+		"gla",
+		"inf",
+		"ita",
+		"kem",
+		"lum",
+		"mat",
+		"nem",
+		"ped",
+		"psh",
+		"rus",
+		"slo",
+		"soc",
+		"spa",
+		"svz",
+		"uzg",
+		"zgo"
+	];
 
-if (!in_array($_POST['predmet'], $predmeti)) {
-	echo "Predmet ne obstaja.";
-	exit();
-}
-
-$statement = $conn->prepare("SELECT code,used FROM codes WHERE code = ?");
-$statement->bind_param('i', $_POST["code"]);
-$statement->execute();
-$statement->store_result();
-$rows = $statement->num_rows;
-if ($rows == 0) {
-	echo "Koda ne obstaja.";
-	exit();
-}
-$statement->bind_result($code,$used);
-while ($statement->fetch()) {
-	if ($used == true) {
-		echo "Koda je že bila uporabljena.";
+	if (!in_array($_POST['predmet'], $predmeti)) {
+		echo "Predmet ne obstaja. Preizkušaš varnost sistema? Incident je zabeležen.";
+		// https://xkcd.com/838/
+		echo '<br><br><a href="../index.html">Domov</a>';
 		exit();
 	}
-}
 
-$statement = $conn->prepare("SELECT * FROM submissions");
-$statement->execute();
-$statement->store_result();
-$rows = $statement->num_rows;
-$statement->close();
+	$statement = $conn->prepare("SELECT code,used FROM codes WHERE code = ?");
+	$statement->bind_param('i', $_POST["code"]);
+	$statement->execute();
+	$statement->store_result();
+	$rows = $statement->num_rows;
+	if ($rows == 0) {
+		echo "Koda ne obstaja.";
+		echo '<br><br><a href="../index.html">Domov</a>';
+		exit();
+	}
+	$statement->bind_result($code,$used);
+	while ($statement->fetch()) {
+		if ($used == true) {
+			echo "Koda je že bila uporabljena.";
+			echo '<br><br><a href="../index.html">Domov</a>';
+			exit();
+		}
+	}
 
-$directory = "../files/";
-$filecount = 0;
-$files = glob($directory . "*");
-if ($files){
-	$filecount = count($files);
-}
+	$statement = $conn->prepare("SELECT * FROM submissions");
+	$statement->execute();
+	$statement->store_result();
+	$rows = $statement->num_rows;
+	$statement->close();
 
-$file = $_FILES['file'];
-$extension = pathinfo($file['name'])["extension"];
-$temp = $file['tmp_name'];
-$filename = "zapiski_".$_POST["predmet"]."_".($filecount+1).".".$extension;
-move_uploaded_file($temp, "../files/".$filename);
+	$directory = "../files/";
+	$filecount = 0;
+	$files = glob($directory . "*");
+	if ($files){
+		$filecount = count($files);
+	}
 
-$ukaz = "INSERT INTO submissions (author, professor, predmet, letnik, tags, datum, title, filename, code)
-		 VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?)";
+	$file = $_FILES['file'];
+	$extension = pathinfo($file['name'])["extension"];
+	$temp = $file['tmp_name'];
+	$filename = "zapiski_".$_POST["predmet"]."_".($filecount+1).".".$extension;
+	move_uploaded_file($temp, "../files/".$filename);
 
-$statement = $conn->prepare($ukaz);
+	$ukaz = "INSERT INTO submissions (author, professor, predmet, letnik, tags, datum, title, filename, code)
+			 VALUES (?, ?, ?, ?, ?, NOW(), ?, ?, ?)";
 
-$statement->bind_param('sssisssi', $_POST["author"],
-								   $_POST["professor"],
-								   $_POST["predmet"],
-								   $_POST["letnik"],
-								   $_POST["tags"],
-								   $_POST["title"],
-								   $filename,
-							  	   $_POST["code"]);
-echo $_POST["author"]."<br>". $_POST["professor"]."<br>".$_POST["predmet"]."<br>".$_POST["letnik"]."<br>".$_POST["tags"]."<br>".$_POST["title"]."<br>";
+	$statement = $conn->prepare($ukaz);
 
-$statement->execute();
+	$statement->bind_param('sssisssi', $_POST["author"],
+									   $_POST["professor"],
+									   $_POST["predmet"],
+									   $_POST["letnik"],
+									   $_POST["tags"],
+									   $_POST["title"],
+									   $filename,
+								  	   $_POST["code"]);
 
-$statement = $conn->prepare("UPDATE codes SET used = true WHERE code = ?");
-$statement->bind_param('i', $_POST["code"]);
-$statement->execute();
+	$statement->execute();
 
-$conn->close();
+	$statement = $conn->prepare("UPDATE codes SET used = true WHERE code = ?");
+	$statement->bind_param('i', $_POST["code"]);
+	$statement->execute();
 
-// only redirect on success
-echo('<!-- Redirect back to index.html -->
-<meta http-equiv = "refresh" content = "0; url=../index.html" />
-<p>Oddaja uspešna. Preusmerjanje ...</p>
-<p>Če se ne zgodi nič, kliknite <a href="../index.html">tukaj</a>.</p>');
+	$conn->close();
+
+	// only redirect on success
+	echo('<!-- Redirect back to index.html -->
+	<meta http-equiv = "refresh" content = "0; url=../index.html" />
+	<p>Oddaja uspešna. Preusmerjanje ...</p>
+	<p>Če se ne zgodi nič, kliknite <a href="../index.html">tukaj</a>.</p>');
 ?>
-<br><br>
-<a href="../index.html">Domov</a>
